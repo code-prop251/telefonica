9f9b5f1a669029e0848ea64a30f68d3eab0ce0bc
Juan David Nupan Guzman <jnupangu@everis.com>
2021-03-10 10:13:36 -0500

    Nuevos campos para equipo

--- agregarEquipo.php
+++ agregarEquipo.php
@@ -19,9 +19,9 @@
         <div>
             <p>Agregar nuevo equipo </p>
         </div>
-        <div>
+        <div >
             <p id="message"></p>
-            <form action="equipos.php?action=add" method="POST">
+            <form action="equipos.php?action=add" method="POST" style="display: flex; flex-direction: column;">
                 <label for="">Serial:</label>
                 <input type="number" name="serial" required>
                 <label for="">Tipo de equipo:</label>
@@ -40,11 +40,40 @@
                     }
                 ?>
                 </select>
+                <label for="">Nombre</label>
+                <input type="text" name="nombre" id="">
                 <label for="">Fecha de vencimiento:</label>
                 <input type="date" name="fecha" id="" value="" required>
+                <label for="">Registro Certificacion</label>
+                <input type="file" name="file" id="">
+                <label for="">Auditado</label>
+                <label for="Si">Si</label><input type="radio" name="auditado" id="Si">
+                <label for="No">No</label><input type="radio" name="auditado" id="No">
+                <div class="adSi" style="display: flex; flex-direction: column;">
+                    <label for="">Fecha auditoria</label>
+                    <input type="date" name="fechaAuditoria" id="">
+                    <label for="">Estado</label>
+                    <textarea name="estado" id="" cols="30" rows="10"></textarea>
+                </div>
+                <label for="">Equipo calibrado</label>
+                <label for="Si">Si</label><input type="radio" name="calibrado" id="Si">
+                <label for="No">No</label><input type="radio" name="calibrado" id="No">
                 <input type="submit" value="Guardar">
             </form>
         </div>
     </div>
+    <script>
+        let si = document.querySelector('#Si');
+        let no = document.querySelector('#No');
+        let adSi = document.querySelector('.adSi');
+
+        si.addEventListener('click', () => {
+            adSi.style.display = "flex";
+        });
+
+        no.addEventListener('click', () => {
+            adSi.style.display = "none";
+        });
+    </script>
 </body>
 </html>
