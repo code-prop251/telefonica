314010a91860bab7c6615566c01dab04e4c3b519
Juan David Nupan Guzman <jnupangu@everis.com>
2021-03-15 10:53:45 -0500

    validaciones para formulario agregar equipos

--- agregarEquipo.php
+++ agregarEquipo.php
@@ -47,6 +47,7 @@
                 <input type="date" name="fecha" id="" value="" required>
                 <label for="">Registro Certificacion</label>
                 <input type="file" name="file" id="file">
+                <button style="width: 100px;" id="clean">Limpiar</button>
                 <label for="">Auditado</label>
                 <select name="auditado" id="aud" required>
                     <option value="" disabled selected>Seleccione</option>
@@ -59,18 +60,18 @@
                     <label for="">Estado</label>
                     <textarea name="estado" id="estado" cols="30" rows="10" required></textarea>
                     <label for="">Revisión de la carpeta del equipo</label>
-                    <textarea name="revision" id="revision" cols="30" rows="10"></textarea>
+                    <textarea name="revision" id="revision" cols="30" rows="10" required></textarea>
                 </div>
                 <label for="">Equipo calibrado</label>
 
                 <div class="caSi">
-                    <select name="calibrado" id="cal" required>
+                    <select name="calibrado" id="cal">
                         <option value="" disabled selected>Seleccione</option>
                         <option value="0">Si</option>
                         <option value="1">No</option>
                     </select>
                 </div>
-                <input type="submit" value="Guardar">
+                <input id="save" type="submit" value="Guardar">
             </form>
         </div>
     </div>
@@ -79,17 +80,31 @@
         let adSi = document.querySelector('.adSi');
         let file = document.querySelector('#file');
         let caSi = document.querySelector('.caSi');
-
+        let save = document.querySelector('#save');
+        let cal = document.querySelector('#cal');
+        let clean = document.querySelector('#clean');
         
+        save.addEventListener('click', (e) => {
+            if(file.value != ''){
+                cal.setAttribute("required","true");
+            }
+        });
+
+        clean.addEventListener('click', (e) => {
+            e.preventDefault();
+            file.value = '';
+        });
 
         aud.addEventListener('change', () => {
             if(aud.value == 0){
                 adSi.style.display = "flex";
                 document.querySelector('#fechaA').setAttribute('required','true');
                 document.querySelector('#estado').setAttribute('required','true');
+                document.querySelector('#revision').setAttribute('required','true');
             }else{
                 document.querySelector('#fechaA').removeAttribute('required');
                 document.querySelector('#estado').removeAttribute('required');
+                document.querySelector('#revision').removeAttribute('required');
                 adSi.style.display = "none";
             }
         });
